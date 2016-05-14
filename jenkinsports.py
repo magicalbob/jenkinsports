4e4520ab11a0a7cb7b10d8bc31eea28b9e0c3c41
Ian Ellis <ellisia@me.com>
2016-05-14 19:36:07 +0100

    modularise

--- jenkinsports.py
+++ jenkinsports.py
@@ -4,27 +4,34 @@ import yaml
 import sys
 import argparse
 
-parser = argparse.ArgumentParser(description='Jenkins Port Register')
-parser.add_argument('-f','--file',help='Register file')
-parser.add_argument('job name',nargs=1,help='Jenkins Job Name')
-args = parser.parse_args()
+def commandArgs():
+  parser = argparse.ArgumentParser(description='Jenkins Port Register')
+  parser.add_argument('-f','--file',help='Register file')
+  parser.add_argument('job name',nargs=1,help='Jenkins Job Name')
+  args = parser.parse_args()
+  return args
 
-if vars(args)['file'] == None:
-  conf_file='jenkinsports.yml'
-else:
-  conf_file=vars(args)['file']
+def jenkinsPorts(args):
+  if vars(args)['file'] == None:
+    conf_file='jenkinsports.yml'
+  else:
+    conf_file=vars(args)['file']
+  
+  try:
+    with open(conf_file,'r') as confile:
+      conf = yaml.load(confile)
+  except:
+    print "Error: opening config file " + conf_file
+    sys.exit(1)
+  
+  for job in conf:
+    if str(job) == str(vars(args)['job name'][0]):
+      for port_no in conf[job]:
+        print "export " + port_no + "=" + conf[job][port_no]
+      sys.exit(0)
+  
+  sys.exit(2)
 
-try:
-  with open(conf_file,'r') as confile:
-    conf = yaml.load(confile)
-except:
-  print "Error: opening config file " + conf_file
-  sys.exit(1)
-
-for job in conf:
-  if str(job) == str(vars(args)['job name'][0]):
-    for port_no in conf[job]:
-      print "export " + port_no + "=" + conf[job][port_no]
-    sys.exit(0)
-
-sys.exit(2)
+if __name__ == '__main__':
+  args=commandArgs()
+  jenkinsPorts(args)
